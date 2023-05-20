153652319ae2d0b843c4b7417151ee3583b01b44
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-20 20:11:28 +0200

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,75 @@
-<template>
-    <nav class="flex space-x-4">
-        <a href="/"
-            class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
-        <a href="/about"
-            class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">About me</a>
+
+    <!-- <nav class="flex space-x-4">
+        <a href="/" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+        <a href="/about" class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">About
+            me</a>
         <a href="/projects"
             class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a>
         <a href="/contact"
             class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Contact</a>
-    </nav>
+    </nav> -->
+<template>
+    <div class="sticky top-0 bg-white rounded-lg">
+        <nav class="container px-6 py-8 mx-auto md:flex md:justify-between md:items-center">
+            <div class="flex items-center justify-between">
+                <RouterLink to="/" class="text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400">
+                    Logo
+                </RouterLink>
+                <div @click="showMenu = !showMenu" class="flex md:hidden">
+                    <button type="button" class="text-gray-800 hover:text-gray-400 focus:outline-none focus:text-gray-400">
+                        <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                            <path fill-rule="evenodd"
+                                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+                            </path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+
+            <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+            <ul :class="showMenu ? 'flex' : 'hidden'"
+                class="flex-col mt-8 space-y-2 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+                <RouterLink :to="'/'">
+                    <li class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                        Home
+                    </li>
+                </RouterLink>
+                <RouterLink :to="'/about'">
+                    <li class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                        About
+                    </li>
+                </RouterLink>
+                <RouterLink :to="'/projects'">
+                    <li class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                        Projects
+                    </li>
+                </RouterLink>
+                <RouterLink :to="'/contact'">
+                    <li class="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+                        Contact
+                    </li>
+                </RouterLink>
+                <div class="flex justify-between items-center hidden sm:block">
+                    <a target="_blank" href="https://github.com/jarvin-s/hc-group-app"
+                        className="ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path
+                                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                        </svg>
+                    </a>
+                </div>
+            </ul>
+        </nav>
+    </div>
 </template>
 <script>
+import { RouterLink } from 'vue-router'
 export default {
-
-}
+    data() {
+        return {
+            showMenu: false,
+        };
+    },
+};
 </script>
