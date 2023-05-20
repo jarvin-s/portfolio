8e7ccc1b79360cd6e1478e51d19f015cc1ffef9e
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-05-20 20:11:58 +0200

    Add Contact and Projects pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident dicta praesentium, natus doloremque rerum ipsum
-        perferendis iste dolorem ullam ex cumque qui non harum aspernatur accusamus obcaecati deleniti aut omnis.</p>
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident dicta praesentium, natus doloremque rerum
+            ipsum
+            perferendis iste dolorem ullam ex cumque qui non harum aspernatur accusamus obcaecati deleniti aut omnis.</p>
+    </div>
 </template>
---
+++ src/views/Projects.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1 class="text-3xl font-bold underline">Hello, Projects</h1>
+</template>
+
+<script setup>
+</script>
