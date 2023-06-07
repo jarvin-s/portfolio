cef0fbb402f42554c09289ac74e8b48b40c1c359
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-07 14:49:17 +0200

    Add autoscroll skills home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,26 +6,38 @@
                     <div class="hero-img"></div>
                     <div class="hero-text">
                         <h1 class="font-bold font-sans dark:text-slate-300"><span
-                                class="container text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-rose-400">Jarvin
+                                class="container text-transparent flex flex-row sm:flex-col bg-clip-text bg-gradient-to-r from-blue-700 to-rose-400">Jarvin
                                 Siegers</span></h1>
                         <h1 class="font-bold font-sans dark:text-white">Software Developer</h1>
                         <p class="text-xl font-sans dark:text-white">I'm a software development student based in
                             <span class="country-span font-bold">The Netherlands.</span>
                         </p>
-                        <ul class="flex flex-row">
-                            <li><img class="w-10" src="/languages/HTML.svg" /></li>
-                            <li><img class="w-10" src="/languages/CSS.svg" /></li>
-                            <li><img class="w-10" src="/languages/JavaScript.svg" /></li>
-                            <li><img class="w-10" src="/languages/Vue.svg" /></li>
-                            <li><img class="w-10" src="/languages/tailwind.svg" /></li>
-                            <li><img class="w-10" src="/languages/git.svg" /></li>
-                            <li><img class="w-10" src="/languages/mysql.svg" /></li>
-                        </ul>
+                        <div class="flex flex-row overflow-hidden items-center mt-6">
+                            <div class="scroll-parent">
+                                <div class="scroll-element primary">
+                                    <img class="w-10" src="/languages/HTML.svg" />
+                                    <img class="w-10" src="/languages/CSS.svg" />
+                                    <img class="w-10" src="/languages/JavaScript.svg" />
+                                    <img class="w-10" src="/languages/Vue.svg" />
+                                    <img class="w-10" src="/languages/tailwind.svg" />
+                                    <img class="w-10" src="/languages/git.svg" />
+                                    <img class="w-10" src="/languages/mysql.svg" />
+                                </div>
+                                <div class="scroll-element secondary">
+                                    <img class="w-10" src="/languages/HTML.svg" />
+                                    <img class="w-10" src="/languages/CSS.svg" />
+                                    <img class="w-10" src="/languages/JavaScript.svg" />
+                                    <img class="w-10" src="/languages/Vue.svg" />
+                                    <img class="w-10" src="/languages/tailwind.svg" />
+                                    <img class="w-10" src="/languages/git.svg" />
+                                    <img class="w-10" src="/languages/mysql.svg" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </section>
 </template>
 
@@ -74,4 +86,70 @@
     -webkit-text-fill-color: transparent;
 }
 
+/* Infinite scroll animation */
+.scroll-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    overflow: hidden;
+    margin-top: 2rem;
+}
+
+.scroll-parent {
+    position: absolute;
+    width: 28.3vw;
+    height: 20rem;
+    padding: 2rem 0;
+    overflow-x: hidden;
+}
+
+.scroll-element {
+    width: inherit;
+    height: inherit;
+    position: absolute;
+    right: 0%;
+    top: 0%;
+    animation: primary 12s linear infinite;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.scroll-element img {
+    width: 6%;
+}
+
+@media screen and (max-width: 768px) {
+    .scroll-element img {
+        width: 20%;
+    }
+}
+
+.primary {
+    animation: primary 12s linear infinite;
+}
+
+.secondary {
+    animation: secondary 12s linear infinite;
+}
+
+@keyframes primary {
+    from {
+        right: 0%;
+    }
+
+    to {
+        right: -100%;
+    }
+}
+
+@keyframes secondary {
+    from {
+        right: 100%;
+    }
+
+    to {
+        right: 0%;
+    }
+}
 </style>
