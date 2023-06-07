b4c26d8884c5aae2a53aa611092f8cf3b7af32b4
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-07 13:45:24 +0200

    Add gradients to home (introduction)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
-    <section id="home" class="hero bg-slate-100 dark:bg-slate-900">
+    <section id="home" class="hero">
         <div class="container mt-10 py-6 justify-center">
             <div class="content flex justify-center items-center gap-x-12 flex-col md:flex-row">
                 <div class="hero-main flex justify-center items-center gap-10 flex-col md:flex-row relative">
                     <div class="hero-img"></div>
                     <div class="hero-text">
-                        <h1 class="font-bold font-sans dark:text-slate-300">Software Developer</h1>
-                        <p class="text-gray-900 font-sans dark:text-slate-100">My name is Jarvin Siegers and I'm a software
-                            development student.</p>
+                        <h1 class="font-bold font-sans dark:text-slate-300"><span
+                                class="container text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-rose-400">Jarvin
+                                Siegers</span></h1>
+                        <h1 class="font-bold font-sans dark:text-white">Software Developer</h1>
+                        <p class="text-xl font-sans dark:text-white">I'm a software development student based in
+                            <span class="country-span font-bold">The Netherlands.</span>
+                        </p>
                         <ul class="flex flex-row">
                             <li><img class="w-10" src="/languages/HTML.svg" /></li>
                             <li><img class="w-10" src="/languages/CSS.svg" /></li>
@@ -21,18 +25,11 @@
                 </div>
             </div>
         </div>
+
     </section>
 </template>
 
 <style>
-/* .content {
-    display: flex;
-    align-items: center;
-    gap: 10rem;
-    justify-content: center;
-    position: relative;
-} */
-
 .hero-img {
     -webkit-animation: morph 6s ease-in-out infinite;
     animation: morph 6s ease-in-out infinite;
@@ -65,5 +62,16 @@
     100% {
         border-radius: 60% 40% 30% 70%/60% 30% 70% 40%;
     }
+
 }
+
+.country-span {
+    color: #d10d16;
+    background-image: -webkit-linear-gradient(270deg, #d10d16 46%, #FFFFFF 64%, #1e63c9 0%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    text-fill-color: transparent;
+    -webkit-text-fill-color: transparent;
+}
+
 </style>
