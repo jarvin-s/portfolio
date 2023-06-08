99566f14297d801a9e55d135f8e3ef81abae6dd2
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-09 00:46:03 +0200

    Fix DarkMode button size

--- src/components/DarkModeToggle.vue
+++ src/components/DarkModeToggle.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-row space-x-8">
         <a target="_blank" href="https://github.com/jarvin-s/portfolio"
-            className="mt-3 ml-6 block text-slate-400 hover:text-slate-500 dark:text-slate-200 dark:hover:text-sky-300">
+            className="mt-1.5 ml-6 block text-slate-400 hover:text-slate-500 dark:text-slate-200 dark:hover:text-sky-300">
             <svg class="dark:fill-zinc-300 dark:hover:fill-zinc-400 hover:fill-zinc-600" xmlns="http://www.w3.org/2000/svg"
                 width="24" height="24" viewBox="0 0 24 24">
                 <path
@@ -12,14 +12,14 @@
             class="p-2 dark:bg-white hover:bg-neutral-500 dark:hover:bg-slate-300 transition duration-300 ease-in-out"
             @click="toggleDark()">
             <div class="dark:flex dark:color-white dark:fill-zinc-100 hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="dark:fill-black w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="dark:fill-black w-5 h-5">
                     <path
                         d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
                 </svg>
             </div>
 
             <div class="dark:hidden flex">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-zinc-200 w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-zinc-200 w-5 h-4">
                     <path fill-rule="evenodd"
                         d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
                         clip-rule="evenodd" />
