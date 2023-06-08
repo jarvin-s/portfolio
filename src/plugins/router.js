0444a1d5559ba113c1f051c1fee999fbcf266a30
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-08 10:11:11 +0200

    Remove About from router

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -7,11 +7,6 @@ const routes = [
         component: () => import('@/views/Home.vue')
     },
     {
-        path: '/about',
-        name: 'about',
-        component: () => import('@/views/About.vue')
-    },
-    {
         path: '/projects',
         name: 'projects',
         component: () => import('@/views/Projects.vue')
