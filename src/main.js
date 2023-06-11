b39a0d30baaa58d09bd9994fe00b25d36a5ae1ea
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-12 00:02:49 +0200

    Add Motion to mainjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '@/style.css'
 import router from '@/plugins/router'
 import { createApp } from 'vue'
+import { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(MotionPlugin).use(router).mount('#app')
