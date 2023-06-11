3a033510ae6ca9e167bdb1e0a56acd062196c96d
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-11 21:27:32 +0200

    Fix Navbar svgs

--- src/components/DarkModeToggle.vue
+++ src/components/DarkModeToggle.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-row space-x-8">
         <a target="_blank" href="https://github.com/jarvin-s/portfolio"
             className="mt-1.5 ml-6 block text-slate-400 hover:text-slate-500 dark:text-slate-200 dark:hover:text-sky-300">
-            <svg class="dark:fill-zinc-300 dark:hover:fill-zinc-400 hover:fill-zinc-600" xmlns="http://www.w3.org/2000/svg"
+            <svg class="dark:fill-zinc-300 dark:hover:fill-zinc-200 dark:fill-white hover:fill-zinc-500" xmlns="http://www.w3.org/2000/svg"
                 width="24" height="24" viewBox="0 0 24 24">
                 <path
                     d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
@@ -19,7 +19,7 @@
             </div>
 
             <div class="dark:hidden flex">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-zinc-200 w-5 h-4">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="fill-zinc-100 w-5 h-4">
                     <path fill-rule="evenodd"
                         d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
                         clip-rule="evenodd" />
