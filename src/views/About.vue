7be429c72dd69d49bc173f78c0b7df392a637766
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-12 10:42:04 +0200

    Fix About text in dark mode

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
     <div class="target" v-motion-slide-visible-once-left>
         <section class="contact bg-white dark:bg-zinc-900">
             <div class="hero-main pb-[500px]">
-                <h1 class="font-bold flex flex-col text-4xl sm:flex-row">dfgkjldsfjlkgdgfh</h1>
+                <h1 class="font-bold flex flex-col dark:text-white text-4xl sm:flex-row">dfgkjldsfjlkgdgfh</h1>
             </div>
         </section>
     </div>
