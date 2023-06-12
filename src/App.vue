221c36a3fed40b4f78ce0a15e7ffa4c63119938f
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-12 11:39:53 +0200

    Remove fade styles

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,4 @@ import Footer from '@/views/Footer.vue'
 </script>
 
 <style>
-.fade-enter-form,
-.fade-leave-to {
-  opacity: 0;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5 ease-out;
-}
 </style>
