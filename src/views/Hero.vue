8815f0db85ff9b411da929cd32800fa2cea32faa
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-15 01:28:20 +0200

    Add Iconify & GitHub code icon to projects

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -147,7 +147,6 @@
 
 <script>
 import { useMotion } from '@vueuse/motion'
-import { Icon } from '@iconify/vue'
 import { ref } from 'vue'
 const targetEl = ref()
 
@@ -322,13 +321,9 @@ useMotion(targetEl, {
 
 .wave {
     animation-name: wave-animation;
-    /* Refers to the name of your @keyframes element below */
     animation-duration: 2.5s;
-    /* Change to speed up or slow down */
     animation-iteration-count: infinite;
-    /* Never stop waving :) */
     transform-origin: 70% 70%;
-    /* Pivot around the bottom-left palm */
     display: inline-block;
 }
 
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -32,28 +32,24 @@
     </section> -->
     <div class="target" v-motion-slide-visible-once-right>
         <section class="bg-gray-100 dark:bg-zinc-800 px-12">
-            <!-- Card Grid -->
             <div class="grid grid-flow gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                <!-- Card Item -->
                 <div v-for="repo in repos" :key="repo.id"
-                    class="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1">
-                    <!-- Clickable Area -->
+                    class="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300">
                     <a _href="link" class="cursor-pointer">
                         <figure>
-                            <!-- Image -->
                             <img v-if="repo.name === repo.name" :src="`/my-projects/${repo.name}.png`"
                                 class="rounded-t h-72 w-full object-cover" />
                         </figure>
                     </a>
-                    <!-- Title -->
                     <p class="text-lg ml-4 mt-4 h-8 mb-4 font-bold leading-relaxed text-gray-800 dark:text-gray-300">
-                        {{ repo.name }}<!-- Post Title -->
+                        {{ repo.name }}
                     </p>
-                    <!-- Description -->
-                    <small class="leading-5 text-gray-500 dark:text-gray-400" x-text="post.description">
-                        <!-- Post Description -->
-                    </small>
-
+                    <div class="flex flex-col sm:flex-row">
+                            <p>Code</p>
+                            <a target="_blank" :href="`${repo.html_url}`">
+                                <Icon icon="mdi:github"  width="28" :inline="true" />
+                            </a>
+                    </div>
                 </div>
             </div>
         </section>
@@ -61,7 +57,12 @@
 </template>
 
 <script>
+import { Icon } from '@iconify/vue';
+
 export default {
+    components: {
+        Icon,
+    },
     data() {
         return {
             repos: [],
