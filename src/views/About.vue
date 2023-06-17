1569c18ccb7db856de599c6314fe15307db3ef99
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-17 15:41:26 +0200

    Update all pages for main branch

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
     <div class="target" v-motion-slide-visible-once-left>
         <section class="about bg-white dark:bg-zinc-900">
             <div class="hero-main pb-[500px]">
-                <h1 class="font-bold flex flex-col dark:text-white text-4xl sm:flex-row">dfgkjldsfjlkgdgfh</h1>
+                <h1 class="font-bold flex flex-col dark:text-white text-4xl sm:flex-row"></h1>
             </div>
         </section>
     </div>
