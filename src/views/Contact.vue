d6122cda15a4e1ccab7b169e6487ef41850e6566
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2023-06-20 14:57:03 +0200

    Rework Contact entirely

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,45 +1,31 @@
 <template>
     <div class="target" v-motion-slide-visible-once-left>
-        <section
-            class="contact border-4 bg-white dark:bg-zinc-900 pt-20 pb-20 flex relative justify-center items-center w-full">
-            <form class="bg-zinc-300 w-1/3 dark:bg-black rounded-lg shadow-lg px-8 pt-6 pb-8 mb-4">
-                <Icon class="flex" icon="material-symbols:contact-page"/><h2 class="mb-4 text-2xl text-black dark:text-white font-semibold">Contact</h2>
-                <div class="mb-4">
-                    <label class="block text-black dark:text-white text-sm font-bold mb-2" for="firstname">
-                        First name
-                    </label>
-                    <input
-                        class="shadow-sm border border-black dark:border-white rounded w-full py-3 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="firstname" type="text" placeholder="First name" />
+        <section class="contact pt-20 pb-20">
+            <form
+                class="bg-zinc-300 space-y-8 py-8 lg:py-16 mx-auto max-w-screen-sm dark:bg-black rounded-lg shadow-lg px-8 pt-6 mb-4">  
+                <h1 class="mb-4 text-4xl text-black text-center dark:text-white font-semibold">Contact</h1>
+                <div>
+                    <label for="name" class="block mb-2 text-sm font-medium text-black dark:text-white">Name</label>
+                    <input type="name" id="name"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                        placeholder="Your full name" required>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-black dark:text-white text-sm font-bold mb-2" for="lastname">
-                        Last name
-                    </label>
-                    <input
-                        class="shadow-sm border border-black dark:border-white rounded  w-full py-3 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username" type="text" placeholder="Last name" />
+                <div>
+                    <label for="email" class="block mb-2 text-sm font-medium text-black dark:text-white">Email</label>
+                    <input type="text" id="email"
+                        class="block p-3 w-full text-sm text-black bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                        placeholder="example@domain.com" required>
                 </div>
-                <div class="mb-4">
-                    <label class="block text-black dark:text-white text-sm font-bold mb-2" for="email">
-                        Email address
-                    </label>
-                    <input
-                        class="shadow-sm border border-black dark:border-white rounded  w-full py-3 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="email" type="email" placeholder="Email address" />
-                </div>
-                <div class="mb-4">
-                    <label class="block text-black dark:text-white text-sm font-bold mb-2" for="message">
-                        Message
-                    </label>
-                    <textarea
-                        class="shadow-sm border border-black dark:border-white rounded  w-full py-3 px-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="message" type="text" placeholder="Your message"></textarea>
+                <div class="sm:col-span-2">
+                    <label for="message" class="block mb-2 text-sm font-medium text-black dark:text-white">Message</label>
+                    <textarea id="message" rows="6"
+                        class="block p-2.5 w-full text-sm text-black bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Your message"></textarea>
                 </div>
                 <button
-                    class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    class="btn-send text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
                     type="button">
-                    Submit
+                    Send message
                 </button>
             </form>
         </section>
@@ -50,4 +36,28 @@
 import { Icon } from '@iconify/vue'
 </script>
 
-<style scoped></style>
+<style scoped>
+/* .btn-send {
+    background: linear-gradient(60deg, #f85923, #f94853, #f99831);
+    transition: 0.3s;
+}
+
+.btn-send:hover {
+    background: linear-gradient(60deg, #772307, #141fc1, #30d512);
+    opacity: 0.8;
+} */
+
+.btn-send {
+  box-shadow: 0 0 6px 0 rgba(157, 96, 212, 2);
+  border: solid 3px transparent;
+  background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), linear-gradient(60deg, #a80195, #e72656e0);
+  background-origin: border-box;
+  box-shadow: 2px 1000px 1px black inset;
+  color: white;
+  transition: 300ms;
+}
+
+.btn-send:hover {
+  box-shadow: none;
+}
+</style>
